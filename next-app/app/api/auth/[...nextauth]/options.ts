2645954523af39e85f007eca7c2127cde096f534
= <kanikayadav061@gmail.com>
2025-11-05 11:35:54 +0530

    jwt and token taken in account

--- next-app/app/api/auth/[...nextauth]/options.ts
+++ next-app/app/api/auth/[...nextauth]/options.ts
@@ -3,12 +3,16 @@ import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { generateKey } from "crypto";
 
+import { JWTPayload, SignJWT, importJWK } from 'jose';
+
 import { JWT } from "next-auth/jwt";
 
 import prisma from "@/app/lib/db";
 
 import bcrypt from 'bcrypt';
 import { randomUUID } from 'crypto'
+import { create } from "domain";
+import Email from "next-auth/providers/email";
 
 declare module "next-auth" {
   interface Session extends DefaultSession {
@@ -39,9 +43,22 @@ interface token extends JWT {
 
 
 
-async function generateJWT(id:any) {
-    return null
-}
+const generateJWT = async (payload: JWTPayload) => {
+  const secret = process.env.JWT_SECRET || 'secret';
+
+  const jwk = await importJWK({ k: secret, alg: 'HS256', kty: 'oct' });
+
+  const jwt = await new SignJWT({
+    ...payload,
+    iat: Math.floor(Date.now() / 1000),
+    jti: randomUUID(), // Adding a unique jti to ensure each token is unique. This helps generate a unique jwtToken on every login
+  })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setExpirationTime('365d')
+    .sign(jwk);
+
+  return jwt;
+};
 
 async function validationUser(
   email: string,
@@ -128,9 +145,7 @@ export const options: NextAuthOptions = {
                 else return null
                 
             }
-
             const hashPassword = await bcrypt.hash(credentials.password,10);
-
             const userDb = await prisma.user.findFirst({
               where:{
                 name:credentials.username,
@@ -144,56 +159,77 @@ export const options: NextAuthOptions = {
               }
             });
             
-            if (userDb && userDb.password && (await bcrypt.compare(credentials.password,userDb.password))){
+            if (userDb && userDb.password){
 
-              const jwt = await generateJWT({
-                id:userDb.id
-              });
+              const checkPass = await bcrypt.compare(credentials.password,userDb.password)
 
-              await prisma.user.update({
-                where:{
+              if(checkPass){
+                const jwt = await generateJWT({
+                  id:userDb.id
+                });
+  
+                await prisma.user.update({
+                  where:{
+                    id:userDb.id,
+                  },
+  
+                  data:{
+                    token:jwt,
+                  }
+                });
+                return{
                   id:userDb.id,
-                },
-
-                data:{
+                  name:userDb.name,
+                  email:userDb.email,
                   token:jwt,
                 }
-              });
-              return{
-                id:userDb.id,
-                name:userDb.name,
-                email:userDb.email,
-                token:jwt,
+
+              }
+              else{
+                console.log("Invalid password for user:", credentials.username);
+                return null;
               }
             }
 
-            console.log("not in db")
 
+            else{
 
+              console.log("not in db")
+  
+  
+  
+              const newUser = await prisma.user.create({
+                data:{
+                  name:credentials.username,
+                  password:hashPassword,
+                  role:credentials.role,
+                  email:credentials.username+"@gmail.com"
+                }
+              })
+              
+              const jwt = await generateJWT({
+                id:newUser.id
+              })
+  
+              await prisma.user.update({
+                where:{
+                  id:newUser.id,
+                },
+                data:{
+                  token:jwt,
+                }
+              })
+  
+              return {
+                  id: newUser.id,
+                  name: newUser.name,
+                  email: newUser.email,
+                  role: newUser.role,
+                  token: jwt
+              };
+            }
+            
 
-            const newUser = await prisma.user.create({
-              data:{
-                name:credentials.username,
-                password:hashPassword,
-                role:credentials.role
-              }
-            })
-
-
-
-            const jwt = await generateJWT({
-              id:newUser.id
-
-            })
-
-
-            return {
-                id: newUser.id,
-                name: newUser.username,
-                email: newUser.email,
-                role: newUser.role,
-                token: jwt
-            };
 
             
             
@@ -203,33 +239,10 @@ export const options: NextAuthOptions = {
         }
 
 
+    }
 
 
-
-
-        const user = {
-          id: "42",
-          name: "Dave",
-          password: "nextauth",
-          role: "admin",
-        };
-
-        if (
-          credentials?.username === user.name &&
-          credentials?.password === user.password &&
-          credentials?.role === user.role
-        ) {
-          return {
-            id: user.id,
-            name: user.name,
-            role: user.role,
-            email: `${user.name}@quiz.com`,
-          };
-        } else {
-          return null;
-        }
-      },
-    }),
+        
   ],
   secret: process.env.NEXTAUTH_SECRET || "secr3t",
   callbacks: {
